46d24bc1e4ce0ccfcd71141c7f6ce58e90113861
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2025-10-06 15:39:20 +0100

    dorg :P

--- class_exercises_/OOP/DogOOP.py
+++ class_exercises_/OOP/DogOOP.py
@@ -1,6 +1,17 @@
 #dog class exercises
 
 class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speaks(self):
+        return f'{self.name} says "woof"'
+
+rufus = Dog('Rufus', 20)
+fido = Dog('Fido', 10)
+
+class Dog:
     species = 'Canis familiaris'
 
     def __init__(self, name, age):
@@ -11,17 +22,21 @@ class Dog:
         return f'{self.name} is {self.age} years old'
 
     def speak(self, sound):
-        print(f'{self.name} says {sound}')
+        print(f'{self.name} barks {sound}')
 
-roxy = Dog('Roxy', 4)
-ruby = Dog('Ruby', 1)
+class Labrador(Dog):
+    def speak(self, sound = 'Worff potato'):
+        super().speak(sound)
+
+roxy = Labrador('Roxy', 4)
+ruby = Labrador('Ruby', 1)
+mabel = Dog('Mabel', 3)
+mango = Labrador('Mango', 1)
+ben = Labrador('Ben', 15)
 
 #tests
 
-ruby.species = 'dorg'
 print(ruby.species)
 
-print(roxy)
-
-print(ruby.speak('Worf worf'))
+print(ruby.speak())
 
