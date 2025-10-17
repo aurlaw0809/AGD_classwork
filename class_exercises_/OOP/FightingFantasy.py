bfc51638d0d80871185a49be15cd5e87187f9ec8
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2025-10-17 10:29:17 +0100

    DND STATS !!!

--- class_exercises_/OOP/FightingFantasy.py
+++ class_exercises_/OOP/FightingFantasy.py
@@ -21,4 +21,52 @@ class Character:
         self.roll = dice_sum(2)
         self.score = self.roll + self.skill
 
-dragon = Character("Dragoneeeeeeeeeee", 10, 22)
+    def take_hit(self, damage: int=2):
+        self.stamina -= damage
+
+    def fight_round(self, other):
+        self.find_score()
+        other.find_score()
+
+        if self.score > other.score:
+            other.take_hit()
+            return 'win!'
+        elif self.score < other.score:
+            self.take_hit()
+            return 'loss..'
+        else:
+            self.take_hit(1)
+            other.take_hit(1)
+            return 'draw..?'
+
+    def return_character_status(self):
+        return f'{self.name} has {self.stamina} stamina(s) left and skill {self.skill}'
+
+    def return_rolls_status(self):
+        return f'{self.name} rolled {self.roll} for a total score of {self.score}'
+
+    @property
+    def is_dead(self):
+        return self.stamina <= 0
+
+    @is_dead.setter
+    def is_dead(self, dead: bool):
+        if dead:
+            self.stamina = 0
+        else:
+            self.stamina = max(self.stamina, 1)
+
+class NPC(Character):
+    pass
+
+class PC(Character):
+    def __init__(self, name: str, skill: int, stamina: int, luck: int):
+        super().__init__(name, skill, stamina)
+        self.luck = luck
+
+    @classmethod
+    def generate_player_character(cls, name):
+        return cls(name, skill = dice_sum(), stamina = dice_sum(), luck = dice_sum())
+
+dragon = NPC("Dragoneeeeeeeeeee", 10, 22)
+jackie = PC("Jackie", 15, 25)
