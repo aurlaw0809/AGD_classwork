d6472a90e857955cd68cef95fd4d47139582eccb
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2025-11-03 16:03:47 +0000

    Game...? STart!

--- class_exercises_/OOP/FightingFantasy.py
+++ class_exercises_/OOP/FightingFantasy.py
@@ -68,6 +68,43 @@ class PC(Character):
     def generate_player_character(cls, name):
         return cls(name, skill = dice_sum(2), stamina = dice_sum(2), luck = dice_sum(2))
 
-dragon = Character("Dragoneeeeeeeeeee", 10, 22)
-jackie = Character("Jackie", 15, 25)
-zippa = PC.generate_player_character("Zippa")
+    def __repr__(self):
+        return f"Character('{self.name}', skill={self.skill}, stamina={self.stamina}, luck={self.luck})"
+
+zippa = PC.generate_player_character("Zippa :|")
+
+class Game:
+    @classmethod
+    def load_creatures(cls):
+        creatures = [Character("Wild goose :>", 12, 12),
+                     Character("Snek :}", 3, 5),
+                     Character("Box Fish :0", 5, 3),
+                     Character("Leonardo DaVinci :|", 8, 4),
+                     Character("Dragon :P", 8, 12),
+                     Character("Jackie >;D", 10, 7),
+                     Character("Coin man ):)", 12, 4),
+                     Character("Sir Tom :)", 6, 4),
+                     Character("Batman B[", 10, 9),
+                     Character("Batsman BD", 11, 10),
+                     Character("Meghahn :=", 5, 5)
+                     ]
+        return creatures
+
+    def __init__(self):
+        self.opponent = None
+        self.player = None
+        self.round_result = None
+        self.creatures = self.load_creatures()
+
+    def choose_opponent(self):
+        self.opponent = random.choice(self.creatures)
+        self.creatures.remove(self.opponent)
+
+    def set_player(self, player_character):
+        self.player = player_character
+
+    def resolve_fight_round(self):
+        self.round_result = self.player.fight_round(self.opponent)
+
+    def return_character_status(self):
+        return f'{self.player.name} has {self.player.stamina} stamina(s) left and skill {self.player.skill}'
