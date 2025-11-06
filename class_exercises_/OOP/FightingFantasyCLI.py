bc879834b29c843913de20eed72643ff0f5153d6
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2025-11-06 22:43:45 +0000

    OLFANA!!!

---
+++ class_exercises_/OOP/FightingFantasyCLI.py
@@ -0,0 +1,26 @@
+import random
+from class_exercises_.OOP.FightingFantasy import Character, PC, Game
+
+'''
+This is the file where i'm going to be creating the CLI for the game :P'''
+
+print("-"*100)
+print("Greetings traveller and welcome to Fighting Fantasy!! >:D")
+print("-"*100)
+print("This here tale takes place in a magical land of curious creatures and" + "\n" +
+      "fantastical fables called Olfana! A land in a world unlike our own!!!" + "\n" +
+      "(No. This is not plagarism...)" + "\n" + "\n" +
+      "The people of Olfana are frightened by the threat of a growing group" + "\n" +
+      "called Necrosofia, which contains members from all the most dangerous" + "\n" +
+      "species in the realm. Necrosofia possess numerous mysterious powers yet" + "\n" +
+      "undisclosed to the public and to be discovered and destroyed during" + "\n" +
+      "battle. With these, they intend to enact their fiendish limited edition" + "\n" +
+      "plot and conquer Olfana as their own!")
+print("-"*100)
+start = input("Traveller! Will you lead our campaign and decimate out enemies? [y/n] ")
+
+'''
+This is Olfana! A land in a world unlike our own!!
+The people of Olfana are frightened by the threat of demons called Necrosofia
+Necrosofia possess a mysterious power called mind and are plotting to conquer Olfana and enact their fiendish limited edition plot
+In order to stop this plan, the King formed a group of br'''
