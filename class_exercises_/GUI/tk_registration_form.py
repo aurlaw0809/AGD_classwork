dd13b3fa81c62c4c06e515faa48ec67da6ae1080
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2025-11-16 12:23:31 +0000

    start of registration form :)

--- class_exercises_/GUI/tk_registration_form.py
+++ class_exercises_/GUI/tk_registration_form.py
@@ -9,19 +9,99 @@ class RegistrationForm(tk.Tk):
 
         self.title('Registration Form')
         self.registration_frame = RegistrationFormFrame(self)
-        self.background_color_frame = RegistrationFormFrame(self)
+        self.gender_choice_frame = GenderChoiceFrame(self)
+        self.entry_box_frame = EntryBoxFrame(self)
 
-        self.registration_frame.pack(side=tk.LEFT)
-        self.background_color_frame.pack(side=tk.LEFT)
+        self.registration_frame.pack(row=0, column=0)
+        self.entry_box_frame.grid(row=0, column=1)
+        self.gender_choice_frame.grid(row=1, column=1)
 
+#registration left side labels
 class RegistrationFormFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
+        #labels
+        self.title = tk.Label(self, text="Registration Form", justify="left", anchor="w")
+        self.name_label = tk.Label(self, text="Name", justify="left", anchor="w")
+        self.email_label = tk.Label(self, text="Email", justify="left", anchor="w")
+        self.gender_label = tk.Label(self, text="Gender", justify="left", anchor="w")
+        self.country_label = tk.Label(self, text="Country", justify="left", anchor="w")
+        self.programming_label = tk.Label(self, text="Programming", justify="left", anchor="w")
+
+        #submit button
+        self.submit = tk.Button(self, text="Submit",)
+
         self.place_widgets()
 
     def place_widgets(self):
-        settings = {'padx': 10, 'pady': 10, 'sticky': 'nswe'}
+        settings = {'padx': 20, 'pady': 10, 'sticky': 'nswe'}
+
+        self.name_label.grid(row=0,
+                             column=0,
+                             **settings)
+
+        self.email_label.grid(row=1,
+                              column=0,
+                              **settings)
+
+        self.gender_label.grid(row=2,
+                               column=0,
+                               **settings)
+
+        self.country_label.grid(row=3,
+                                column=0,
+                                **settings)
+
+        self.programming_label.grid(row=4,
+                                    column=0,
+                                    **settings)
+#email and name entry buttons
+class EntryBoxFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.name = tk.Entry(self)
+        self.email = tk.Entry(self)
+
+        self.place_widgets()
+
+    def place_widgets(self):
+        settings = {'padx': 20, 'pady': 10, 'sticky': 'nswe'}
+
+        self.name.grid(row=0,
+                       column=0,
+                       **settings)
+
+        self.email.grid(row=1,
+                        column=0,
+                        **settings)
+
+#gender choice buttons
+class GenderChoiceFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        # Gender choices
+        self.options = ['Male', 'Female']
+
+        # Create a tk variable which will hold the value of the selcted gender
+        self.gender_choice = tk.StringVar()
+        self.gender_choice.set(self.options[0])
+
+        # Create radio buttons (list comprehension)
+        self.gender_options = [tk.Radiobutton(self, text=gender,
+                                             value=gender,
+                                             variable=self.gender_choice,
+                                             # command=self.change_color
+                                             )
+                              for gender in self.options]
+
+        self.place_widgets()
+
+    def place_widgets(self):
+        for item in self.gender_options:
+            item.pack(side=tk.LEFT, anchor='w', padx=(5, 10), pady=5)
 
 if __name__ == '__main__':
 
