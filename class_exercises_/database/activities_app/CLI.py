0690850b29b9a5e10d4240389fd329e427932f4a
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2026-01-27 14:52:09 +0000

    midway CLI jazz

--- class_exercises_/database/activities_app/CLI.py
+++ class_exercises_/database/activities_app/CLI.py
@@ -15,9 +15,43 @@ class CLI:
 
     def show_activities_persons(self):
         activity = input('Enter activity: ')
-        participants = self.controller.get_person_activities(activity)
+        participants = self.controller.get_activities_people(activity)
         for participant in participants:
             print(participant)
+    '''
+    def show_all_people(self):
+        people = self.controller.get_all_people()
+        for person in people:
+            print(person)
+
+    def show_all_activities(self):
+        activities = self.controller.get_all_activities()
+        for activity in activities:
+            print(activity)
+    '''
+
+    def add_person(self):
+        first_name = input('Enter first name: ')
+        last_name = input('Enter last name: ')
+        self.controller.add_person(first_name, last_name)
+
+    def delete_person(self):
+        person_id = input('Enter person ID: ')
+        self.controller.delete_person(person_id)
+
+    def add_activity(self):
+        name = input('Enter activity name: ')
+        self.controller.add_activity(name)
+
+    def delete_activity(self):
+        activity_id = input('Enter activity ID: ')
+        self.controller.delete_activity(activity_id)
+
+    def edit_person(self):
+        pass
+
+    def edit_activity(self):
+        pass
 
 
 if __name__ == '__main__':
@@ -26,4 +60,5 @@ if __name__ == '__main__':
 '''
 cli.show_activities_persons()
 Gang gang.
+cli.show_all_people()
 '''
--- class_exercises_/database/activities_app/controller.py
+++ class_exercises_/database/activities_app/controller.py
@@ -3,7 +3,6 @@ import sqlalchemy.orm as so
 
 from models import Person, Activity
 
-
 class Controller:
     def __init__(self, db_location = 'sqlite:///activities.sqlite'):
         self.engine = sa.create_engine(db_location)
@@ -20,9 +19,49 @@ class Controller:
         with so.Session(bind=self.engine) as session:
             stmt = sa.select(Activity).where(Activity.name == activity_name)
             activity = session.scalar(stmt)
-            participants = activity.attendees
-            people_names = [people.first_name for people in participants]
+            peoples = activity.attendees
+            people_names = [{person.first_name, person.last_name} for person in peoples]
         return people_names
+    '''
+    def get_all_people(self):
+        with so.Session(bind=self.engine) as session:
+            stmt = sa.select(Person).order_by(Person.first_name)
+            peoples = session.scalar(stmt)
+            #TODO not working
+            #person_names =[{person.first_name, person.last_name} for person in peoples]
+        return peoples
+
+    def get_all_activities(self):
+        with so.Session(bind=self.engine) as session:
+            stmt = sa.select(Activity.name).order_by(Activity.name)
+            activities = session.execute(stmt)
+            print(activities)
+            return activities
+    '''
+
+    def add_person(self, first_name, last_name):
+        with so.Session(bind=self.engine) as session:
+            person = Person(first_name=first_name, last_name=last_name)
+            session.add(person)
+            session.commit()
+
+    def delete_person(self, person_id):
+        with so.Session(bind=self.engine) as session:
+            person = session.get(Person, person_id)
+            session.delete(person)
+            session.commit()
+
+    def add_activity(self, activity_name):
+        with so.Session(bind=self.engine) as session:
+            activity = Activity(name=activity_name)
+            session.add(activity)
+            session.commit()
+
+    def delete_activity(self, activity_id):
+        with so.Session(bind=self.engine) as session:
+            activity = session.get(Activity, activity_id)
+            session.delete(activity)
+            session.commit()
 
 
 if __name__ == '__main__':
