035ae20fbcbbfd3b9ba9f51d22ace223e2651406
aurlaw0809 <aurlaw0809@highgateschool.org.uk>
2026-03-09 12:35:52 +0000

    cheese maze core again PT2

---
+++ class_exercises_/pygame_mvc/maze_TUI_student.py
@@ -0,0 +1,40 @@
+#from class_exercises_.pygame_mvc.game_controller import Game
+
+
+class TextInterface:
+    """ Create a text-based interface for the turn-based game."""
+    def __init__(self):
+        self.game = Game()
+        self.game.set_up()
+        self.player = self.game.characters[0]
+        self.game_area = []
+        self.running = True
+
+    def _create_area(self):
+        """ Create a list of lists where each [row][col] in self.game_area is given the first letter of the background or character in that grid location. If there is no background or character in a grid location, use the default '.'"""
+        pass
+
+
+    def _draw_area(self):
+        """ Loop through each row, join the characters in that row and print it out 'W' in the grid is replaced by '\u2593' (a gray square), borders of the grid are shown using the unicode box-drawing characters (https://jrgraphix.net/r/Unicode/2500-257F)"""
+        pass
+
+
+    def _handle_input(self):
+        """Ask the user to input a direction and use game.move_character to move in that direction.
+        Set self.running to false if the user enters Q."""
+        pass
+
+
+
+    def main_loop(self):
+        """Keep drawing the area and asking for player moves while self.runnng is True."""
+        print("Welcome to Andrew's Game")
+        while self.running:
+            self._draw_area()
+            self._handle_input()
+
+
+if __name__ == "__main__":
+    tui = TextInterface()
+    tui.main_loop()
